24790a69c85344b4220ba1ccfa9a469828fa1701
IBRAHIM HOSSAIN <ibrahimhossaincse@gmail.com>
2023-12-15 08:14:15 +0600

    navigation bar completed

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { Nav, NavItem, Navbar } from 'react-bootstrap';
 import {
   increment,
   incrementAsync,
   decrement,
   decrementAsync
 } from '../../modules/counter'
+import { Link } from 'react-router-dom';
 
  class Home extends React.Component{
   login = () => {
@@ -15,8 +17,40 @@ import {
   }
   render(){
     return(
-        <div>
-          <h1>Home</h1>
+        <div className='container' style={{marginTop: "10px"}}>
+          <Navbar collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to="/">Task Manager</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} onClick={() => this.props.changePage()}>
+                  Task List
+                </NavItem>
+                {/* <NavItem eventKey={2} href="#">
+                  Link
+                </NavItem> */}
+                {/* <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                  <MenuItem eventKey={3.1}>Action</MenuItem>
+                  <MenuItem eventKey={3.2}>Another action</MenuItem>
+                  <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                </NavDropdown> */}
+              </Nav>
+              <Nav pullRight>
+                <NavItem eventKey={1} onClick={this.login} style={{paddingRight: "20px"}}>
+                  Logout
+                </NavItem>
+                {/* <NavItem eventKey={2} href="#">
+                  Link Right
+                </NavItem> */}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
           <p>Count: {this.props.count}</p>
 
           <p>
