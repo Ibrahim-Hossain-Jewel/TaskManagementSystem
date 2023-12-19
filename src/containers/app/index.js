cb299f0f80d62582dc82c6456691d7c54c82e07e
IBRAHIM HOSSAIN <ibrahimhossaincse@gmail.com>
2023-12-19 18:53:02 +0600

    node version added to the project due to deploy this project

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
 import Home from '../home';
 import SignIn from '../SignIn';
 import Signup from '../signup';
+import TaskList from '../list';
 import { Route, Switch,BrowserRouter,Redirect } from 'react-router-dom'
 
 
@@ -29,7 +29,8 @@ export default () => (
       <Route exact path="/login" render={props=><SignIn {...props} />} />
       <Route exact path="/Signup" render={props=><Signup {...props} />} />
       <AuthRoute exact path="/" component={Home} />
-    </Switch> 
+      <AuthRoute exact path="/tasklist" component={TaskList} />
+    </Switch>
   </BrowserRouter>
   
 )
